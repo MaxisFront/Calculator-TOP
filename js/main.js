bcde6b8cb658464ecf77c7be6c7069e4adf63de5
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-29 18:29:28 -0600

    feat(core): add initial logic for input field
    - Support capturing keys after an interaction with the keypad.
    - Display inputs in the calculator display.
    - Save the input after the key "=" is pressed.

--- js/main.js
+++ js/main.js
@@ -4,17 +4,87 @@
 
   // Global Values;
   let resultsToDisplay = -1;
+  let calculatorInput;
 
   // NodeLists
   /** @type {NodeListOf<HTMLElement>} */
   const outputDivs = document.querySelectorAll(".calculator__output");
   const operationSpans = document.querySelectorAll(".calculator__operations");
-  const equalSigns = document.querySelectorAll('[data-symbol="equal"]');
+  const resultSpans = document.querySelectorAll(".calculator__result");
+  const keypadValues = document.querySelectorAll
+  (".calculator__key--num, .calculator__key--operator");
+  const keypadActions = document.querySelectorAll(".calculator__key--action");
 
   // Nodes
   const mainDisplayText = document.querySelector('[data-result="main"]');
   
-  
+  // Core functionalities
+
+
+
+  // keys Functionality
+
+  keypadValues.forEach(element => {
+    element.addEventListener("click", () => {
+      
+      if (mainDisplayText) mainDisplayText.textContent += element.dataset.key;
+
+    });
+  });
+
+  keypadActions.forEach(element => {
+    element.addEventListener("click", () => {
+      switch (element.dataset.key) {
+        case "del":
+          alert(element.dataset.key);
+          break;
+        case "ac":
+          cleanDisplay();
+          break;
+        case "lol":
+          alert(element.dataset.key);
+          break;
+        case "ans":
+          alert(element.dataset.key);
+          break;
+        case "calculate":
+          calculateResult();
+          break;
+        default:
+          break;
+      }
+
+    });
+  });
+
+  // Calculate Functions
+  function calculateResult() {
+    if (!mainDisplayText?.textContent) return;
+
+    const allowedValues = /^[0-9×÷+.\-]+$/;
+
+    if (!allowedValues.test(mainDisplayText.textContent)) {
+      mainDisplayText.textContent = "Expression not allowed";
+      return;
+    }
+
+    const operators = /([×÷+-])/;
+    const values = mainDisplayText.textContent.split(operators);
+
+    alert(values);
+
+  }
+
+
+
+  /** @param {string} operation 
+   * @param {string} result
+  */
+  function saveOperation(operation, result) {
+
+  }
+
+
 
   // Clen Functions
   function cleanDisplay() {
@@ -25,7 +95,6 @@
   function cleanOutputs() {
 
     operationSpans.forEach(element => element.textContent = "");
-    equalSigns.forEach(element => element.textContent = "");
     outputDivs.forEach((element) => element.style.display = "none");
   }
 
