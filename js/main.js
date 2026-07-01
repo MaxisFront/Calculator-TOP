b836a7d59f8b4fdacab05507f686c7bdee683b9b
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-30 21:23:27 -0600

    feat(core): add calculation and processing logic
    - Process and clean the user input.
    - Add function to calculate the product and quotient.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,20 @@
   // NodeLists
   /** @type {NodeListOf<HTMLElement>} */
   const outputDivs = document.querySelectorAll(".calculator__output");
+  /** @type {HTMLElement | null} */
+  const mainOutputDiv = document.querySelector(".calculator__output--main");
+  /** @type {NodeListOf<HTMLElement>} */
   const operationSpans = document.querySelectorAll(".calculator__operations");
+  /** @type {NodeListOf<HTMLElement>} */
   const resultSpans = document.querySelectorAll(".calculator__result");
+  /** @type {NodeListOf<HTMLElement>} */
   const keypadValues = document.querySelectorAll
   (".calculator__key--num, .calculator__key--operator");
+  /** @type {NodeListOf<HTMLElement>} */
   const keypadActions = document.querySelectorAll(".calculator__key--action");
 
   // Nodes
+  /** @type {HTMLElement | null} */
   const mainDisplayText = document.querySelector('[data-result="main"]');
   
   // Core functionalities
@@ -28,6 +35,7 @@
     element.addEventListener("click", () => {
       
       if (mainDisplayText) mainDisplayText.textContent += element.dataset.key;
+      if (mainOutputDiv && mainOutputDiv.style.display === "none") mainOutputDiv.style.display = "block";
 
     });
   });
@@ -48,7 +56,7 @@
           alert(element.dataset.key);
           break;
         case "calculate":
-          calculateResult();
+          getResult();
           break;
         default:
           break;
@@ -58,9 +66,18 @@
   });
 
   // Calculate Functions
-  function calculateResult() {
-    if (!mainDisplayText?.textContent) return;
+  function getResult() {
 
+    let input = getInput();
+    if (!input || input?.length <= 2) return;
+    
+    const result = calculateResult(input);
+    alert(result);
+  }
+
+  function getInput() {
+    if (!mainDisplayText?.textContent) return;
+    
     const allowedValues = /^[0-9×÷+.\-]+$/;
 
     if (!allowedValues.test(mainDisplayText.textContent)) {
@@ -68,20 +85,70 @@
       return;
     }
 
-    const operators = /([×÷+-])/;
-    const values = mainDisplayText.textContent.split(operators);
-
-    alert(values);
-
+    // Split the displayed values with the operators as limiters.
+    // Ex. "-21+40" becomes ["-21","+","40"] 
+    const operators = /(?<=.)([×÷+-])/;
+    return mainDisplayText.textContent.split(operators).filter(Boolean);   
   }
 
+  /** @param {string[]} cleanInput  */
+  function calculateResult(cleanInput) {
 
+    let input = cleanInput;
+    let result = "";
+
+    while (input.length > 1) {
+
+      const productIndex = input.indexOf("×");
+      const divisionIndex = input.indexOf("÷");
+
+      if (productIndex !== -1 || divisionIndex !== -1) { 
+
+        const nextProduct = productIndex !== -1 ? productIndex : Infinity;
+        const nextDivision = divisionIndex !== -1 ? divisionIndex : Infinity;
+      
+        if (nextProduct <= nextDivision) {
+
+          alert(divisionIndex);
+          alert(productIndex);
+          result = obtainProduct(input[nextProduct - 1], input[nextProduct + 1]);
+          input.splice(nextProduct - 1, 3, String(result));
+
+        } else if (nextDivision < nextProduct) {
+
+          alert("huh");
+          result = obtainQuotient(input[nextDivision - 1], input[nextDivision + 1]);
+          input.splice(divisionIndex - 1, 3, String(result));
+        }
+      }
+    }
+
+    // TODO: Add Addition and subtraction logic
+    // TODO: Display the "result" value at the display
+    // TODO: Add logic for the result history 
+
+    return input[0];
+  }
+
+  /** @param {string} firstValue
+   * @param {string} secondValue 
+  */
+  function obtainProduct(firstValue, secondValue) {
+    return String((Number(firstValue)) * (Number(secondValue)));
+  }
+
+  /** @param {string} firstValue
+   * @param {string} secondValue 
+  */
+  function obtainQuotient(firstValue, secondValue) {
+    return String((Number(firstValue)) / (Number(secondValue)));
+  }
 
   /** @param {string} operation 
    * @param {string} result
   */
   function saveOperation(operation, result) {
-
+    // TODO: Save the operation and the result, and display in the operations history
   }
 
 
@@ -99,7 +166,10 @@
   }
 
   function cleanMainOutput() {
-    if (mainDisplayText) mainDisplayText.textContent = "";
+    if (mainDisplayText && mainOutputDiv) {
+      mainOutputDiv.style.display = "none";
+      mainDisplayText.textContent = "";
+    }
   }
   
   
